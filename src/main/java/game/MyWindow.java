08a86f2ffa50f5dee121f6c0162cede31495f440
IgorLevch <igorlevvv@gmail.com>
2023-09-11 22:09:40 +0300

    The first Commit

---
+++ src/main/java/game/MyWindow.java
@@ -0,0 +1,37 @@
+package game;
+
+import javax.swing.*;
+
+
+public class MyWindow extends JFrame {
+
+    int x1 =10;
+    int y1 = 20;
+    char sym1 ='*';
+
+
+    int x2 =100;
+    int y2 = 110;
+    char sym2 ='&';
+
+    public MyWindow() {
+        setTitle("Snake Game");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(100,200,600,600);
+
+
+        setLayout(null);
+        JButton button = new JButton("Snake");
+        button.setBounds(x1,y1,80,80);
+        add(button);
+
+
+        JButton button2 = new JButton("Snake2");
+        button2.setBounds(x2,y2,80,80);
+        add(button2);
+
+        setVisible(true);
+    }
+
+
+}
